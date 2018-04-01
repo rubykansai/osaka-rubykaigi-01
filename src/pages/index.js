d0390d90f83780def9e3e391a10d3873f032a1c9
ogom <ogom@outlook.com>
2018-04-01 22:04:52 +0900

    Add Register Now to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,12 @@ const IndexPage = () => (
     <h1>大阪Ruby会議2018</h1>
     <p>日付：2018年7月21日（土）</p>
     <p>場所：大阪科学技術センター</p>
+    <p>公演：まつもと ゆきひろ</p>
+    <p>
+      参加：<a href="https://rubykansai.doorkeeper.jp/events/72775">
+        こちらから
+      </a>
+    </p>
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </div>
 )
